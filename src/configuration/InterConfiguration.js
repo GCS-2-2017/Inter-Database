f3942babcb136f5959b20a29400720765a8c07a7
Carolina Ferreira <carol.mf14@gmail.com>
2017-06-17 00:31:16 -0300

    [MYSQL] Added default configuration for MySQL Database
    Signed-off-by: Carolina Ferreira <carol.mf14@gmail.com>
    Signed-off-by: Vitor Borges <vitorbertulucci@gmail.com>

--- src/configuration/InterConfiguration.js
+++ src/configuration/InterConfiguration.js
@@ -1,28 +1,20 @@
 'use strict'
 
 class InterConfiguration {
-  constructor (databaseServer, databaseUser, databasePassword) {
+  constructor(databaseName, databaseServer, databaseUser,
+              databasePassword){
     if (this.constructor === InterConfiguration) {
       throw new TypeError('Can not construct an Abstract class')
-    }
-    else {
+    } else{
+      this.databaseName = databaseName
       this.databaseServer = databaseServer
       this.databaseUser = databaseUser
       this.databasePassword = databasePassword
     }
   }
-    
-  static getConfiguration () {
-    if (this === InterConfiguration) {
-      // Error: Abstract methods can not be called directly.
-      throw new TypeError('Can not call static abstract method getConfiguration')
-    } else if (this.getConfiguration === InterConfiguration.getConfiguration) {
-      // Error: The child has not implemented this method.
-      throw new TypeError('Please implement static abstract method getConfiguration')
-    } else {
-      // Error: The child has implemented this method but also called `super.getConfiguration()`.
-      throw new TypeError('Do not call static abstract method getConfiguration from child')
-    }
+
+  getConnection () {
+    throw new TypeError('Do not call abstract method foo from child.')
   }
 }
 

e95c76a1c297e6bef87b58bd95965e5c72082a40
DaniloBarros <dan.b412@gmail.com>
2017-06-19 11:44:03 -0300

    [STRUCTURING] Structuring files and folders to conform with architecture

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import interDatabase from './inter-database'
-import { Router } from 'express'
-
-export default () => {
-  let app = Router()
-  app.use('/inter-database', interDatabase(app))
-
-  return app
-}

38735bd46fd7c7882163a123785dba5d0546ee24
DaniloBarros <dan.b412@gmail.com>
2017-06-21 23:41:46 -0300

    [INTERDATABASE] Fixed issues in strategy. Modified configuration abstract class to have only one variable config, instead of config and options
    Signed-off-by: DaniloBarros <dan.b412@gmail.com>
    Signed-off-by: Elmar Roberto <elmarunb@gmail.com>

--- test/InterDatabase.js
+++ test/InterDatabase.js
@@ -3,15 +3,18 @@ import InterDatabase from '../lib/consumption/InterDatabase'
 
 const expect = chai.expect
 
-let options
-
 describe('InterDatabase instantiation with arguments', () => {
   it('should accept four parameters (database, user, password, options)', () => {
-    const INTERDB = new InterDatabase ('test', 'root', 'root', {
+    const INTERDB = new InterDatabase('test', 'root', 'root', {
       host: 'localhost',
       type: 'mysql'
     })
-
-    expect(INTERDB.interConfiguration).to.not.be.null
+    const CONFIG = INTERDB.interConfiguration.getConfiguration().config
+    expect(CONFIG).to.not.be.null
+    expect(CONFIG.host).to.be.equal('localhost')
+    expect(CONFIG.user).to.be.equal('root')
+    expect(CONFIG.password).to.be.equal('root')
+    expect(CONFIG.database).to.be.equal('test')
+    // expect(true).to.be.true
   })
 })

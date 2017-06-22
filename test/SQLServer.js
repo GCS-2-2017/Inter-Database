e353d57e33bb08e482ba9db292071080f898a13b
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-06-22 15:37:49 -0300

    [CONNECTION TEST] Adding insert method test of sql server - Fixing connection methods - Changing helper method to ES5 syntax
    Signed-off-by: Thiago Moreira <thiago.cmoreira@yahoo.com.br>
    Signed-off-by: Vitor Bertulucci <vitorbertulucci@gmail.com>

--- test/SQLServer.js
+++ test/SQLServer.js
@@ -1,5 +1,7 @@
 import chai from 'chai'
 import SQLServer from '../lib/configuration/dbs/SQLServer'
+import Connection from '../lib/connection/dbs/SQLServer'
+import { formatObject, formatInsertValues } from '../lib/helpers/connection'
 
 const expect = chai.expect
 
@@ -15,5 +17,27 @@ describe('SQL Server instantiation with arguments', () => {
     expect(SQL_CONNECTION._option.userName).to.be.equal('sa')
     expect(SQL_CONNECTION._option.password).to.be.equal('12345678')
     expect(SQL_CONNECTION._option.server).to.be.equal('177.143.213.117')
+  })
+})
+
+describe('SQL Server connection', () => {
+  it('should use insert method with two parameters (table, object)', async () => {
+    const SQL = new SQLServer('teste', 'sa','12345678', {
+      port: '9990',
+      server: '177.143.213.117'
+    })
+    const SqlServerConnection = new Connection(SQL)
+
+    const workshift = {
+      name: 'Workshift Test',
+      description: 'Workshift Framework Test'
+    }
+
+    try {
+      var result = await SqlServerConnection.insert('[SEQUENCIALIZADOR].[dbo].[WorkShift]', workshift)
+      expect(result).to.be.empty
+    } catch (error) {
+      console.log(error)
+    }
   })
 })

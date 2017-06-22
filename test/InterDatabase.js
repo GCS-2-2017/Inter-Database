d746c68c32f2fd93ddd2d1100bc0b747090604d5
DaniloBarros <dan.b412@gmail.com>
2017-06-22 03:22:41 -0300

    [CONNECTION] Implemented strategy method for Connection, fixed connection abstract and mysql classes.
    Signed-off-by: DaniloBarros <dan.b412@gmail.com>
    Signed-off-by: Gabriel Climaco <gabrielsclimaco@gmail.com>
    Signed-off-by: Elmar Roberto <elmarunb@gmail.com>

--- test/InterDatabase.js
+++ test/InterDatabase.js
@@ -1,10 +1,11 @@
 import chai from 'chai'
 import InterDatabase from '../lib/consumption/InterDatabase'
+import MySQLConnection from '../lib/connection/dbs/MySQL'
 
 const expect = chai.expect
 
-describe('InterDatabase instantiation with arguments', () => {
-  it('should accept four parameters (database, user, password, options)', () => {
+describe('InterDatabase instantiation with arguments (database, user, password, options)', () => {
+  it('should save correct configuration', () => {
     const INTERDB = new InterDatabase('test', 'root', 'root', {
       host: 'localhost',
       type: 'mysql'
@@ -15,6 +16,21 @@ describe('InterDatabase instantiation with arguments', () => {
     expect(CONFIG.user).to.be.equal('root')
     expect(CONFIG.password).to.be.equal('root')
     expect(CONFIG.database).to.be.equal('test')
-    // expect(true).to.be.true
+  })
+
+  it('should save correct connection', () => {
+    const INTERDB = new InterDatabase('test', 'root', 'root', {
+      host: 'localhost',
+      type: 'mysql'
+    })
+    const CONNECTION = INTERDB.interConnection
+    expect(CONNECTION).to.not.be.null
+    expect(CONNECTION.connection).to.exist
+    expect(CONNECTION).to.be.an.instanceof(MySQLConnection)
+
+    expect(CONNECTION.connection.config.host).to.be.equal('localhost')
+    expect(CONNECTION.connection.config.user).to.be.equal('root')
+    expect(CONNECTION.connection.config.password).to.be.equal('root')
+    expect(CONNECTION.connection.config.database).to.be.equal('test')
   })
 })

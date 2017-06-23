8aecfb4c9eabea5d4629f884a68a8d9c232a32f6
Elmar Roberto <elmarunb@gmail.com>
2017-06-23 17:01:07 -0300

    [HOTSPOT] Adding methods to overide in HotSpot on addDB
    Signed-off-by: Cecilia Dib <ceciliabessa@hotmail.com>

--- test/InterDatabase.js
+++ test/InterDatabase.js
@@ -51,19 +51,25 @@ describe('InterDatabase', () => {
       expect(CONFIG._option.password).to.be.equal('root')
       expect(CONFIG._option.server).to.be.equal('192.168.0.1')
     })
+  })
 
-    it('should add correctly a new custom Database', () => {
+  describe('addDB', () => {
+    it('should add correctly a new custom Database', async () => {
       let success = InterDatabase.addDB({
-        select: (table, argument, object) => {
-          console.log('ihuul')
-          return true
+        select: (table, argument, object, thisInstance) => {
+          return {
+            selectedItems: 'Yay',
+            object
+          }
+        },
+
+        selectDistinct: (table, argument, thisInstance) => {
+          return thisInstance.connection.getConfiguration()
         }
       },{
         type: 'sqlite',
-        getConfiguration: () => {
-          console.log('oioioi')
-          console.log(this)
-          return this.config
+        getConfiguration: (thisInstance) => {
+          return thisInstance.config
         }
       })
 
@@ -72,10 +78,36 @@ describe('InterDatabase', () => {
         type: 'sqlite'
       })
 
-      const CONFIG = SQLITE.interConfiguration.config.type
-      console.log(SQLITE.interConfiguration.getConfiguration())
+      const CONFIG = SQLITE.interConfiguration.config
 
       expect(success).to.be.true
+      expect(CONFIG).to.exist
+      expect(CONFIG.type).to.be.equal('sqlite')
+      expect(CONFIG.database).to.be.equal('test')
+      expect(SQLITE.interConfiguration.getConfiguration()).to.be.deep.equal(CONFIG)
+
+      let resultSelect = null
+      let resultSelectDistinct = null
+      let resultTable = null
+      try {
+        resultSelect = await SQLITE.interConnection.select('testTable', 'TestArgument', {test: 'Test'})
+        resultSelectDistinct = await SQLITE.interConnection.selectDistinct('testTable', 'TestArgument')
+        resultTable = await SQLITE.interConnection.table()
+      } catch (err) {
+        resultSelect = err
+        resultSelectDistinct = err
+        resultTable = err
+      }
+      expect(resultSelect).to.not.be.null
+      expect(resultSelectDistinct).to.not.be.null
+      expect(resultTable).to.not.be.null
+
+      expect(resultSelect).to.have.all.keys(['selectedItems', 'object'])
+      expect(resultSelect.selectedItems).to.be.equal('Yay')
+
+      expect(resultSelectDistinct).to.be.deep.equal(CONFIG)
+      expect(resultTable instanceof TypeError).to.be.true
+      expect(resultTable.message).to.be.equal('table() for sqlite not implemented')
     })
   })
 
